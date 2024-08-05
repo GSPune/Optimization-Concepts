dca68bf25a4eea52d8e9d666a2a3f09c1305f109
Stephen George <stephengeorgepune@gmail.com>
2024-08-05 09:50:01 +0530

    Some additions

--- Stochastic Methods/travellingSalesman(SA).py
+++ Stochastic Methods/travellingSalesman(SA).py
@@ -1,5 +1,6 @@
 from geopy.geocoders import Nominatim
 # from numpy import *
+import math
 
 def readcities():
     p = [] #co-ordinates of cities
@@ -21,5 +22,27 @@ def readcities():
                 j += 1
     return p
 
-P = readcities()
-print(f"Number of cities is {len(P)}.")
+# P = readcities()
+# print(f"Number of cities is {len(P)}.")
+
+
+def distance(P1,P2):
+    d = math.sqrt((P1[0]-P2[0])**2 + (P1[1]-P2[1])**2)
+    return d
+
+
+d1 = distance([23.0215374,72.5800568],[31.6343083,74.8736788])
+print(d1)
+
+def totalDistance(P,seq):
+    dist = 0.0
+    N = len(seq)
+    for i in range(0,N-1):
+        dist += distance(P[seq[i]],P[seq[i+1]])
+    
+    dist += distance(P[0],P[seq[N-1]])
+    return dist
+
+def plot():
+    pass
+
