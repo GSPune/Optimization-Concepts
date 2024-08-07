918502a76647f9f8b8b453e1c89fcb5396e73b25
Stephen George <stephengeorgepune@gmail.com>
2024-08-07 22:02:18 +0530

    Some changes

--- Stochastic Methods/travellingSalesman(SA).py
+++ Stochastic Methods/travellingSalesman(SA).py
@@ -1,8 +1,9 @@
 from geopy.geocoders import Nominatim
-# from numpy import *
+from numpy import *
+from pylab import *
 import math
 
-def readcities():
+def readcities(Pnames):
     p = [] #co-ordinates of cities
     j = 0 #counter
     geolocator = Nominatim(user_agent="SG_App")
@@ -13,6 +14,7 @@ def readcities():
                 city = line.rstrip("\n")
                 if(city == ""):
                     break
+                Pnames.insert(j,city)
                 city += ", India"
                 pt = geolocator.geocode(city,timeout=10000)
                 #  out.write("City = ",city,pt.latitude,pt.longitude)
@@ -22,8 +24,9 @@ def readcities():
                 j += 1
     return p
 
-# P = readcities()
-# print(f"Number of cities is {len(P)}.")
+Pnames = []
+P = readcities(Pnames)
+print(f"Number of cities is {len(P)}.")
 
 
 def distance(P1,P2):
@@ -31,8 +34,8 @@ def distance(P1,P2):
     return d
 
 
-d1 = distance([23.0215374,72.5800568],[31.6343083,74.8736788])
-print(d1)
+#d1 = distance([23.0215374,72.5800568],[31.6343083,74.8736788])
+#print(d1)
 
 def totalDistance(P,seq):
     dist = 0.0
@@ -43,6 +46,17 @@ def totalDistance(P,seq):
     dist += distance(P[0],P[seq[N-1]])
     return dist
 
-def plot():
-    pass
+s = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+#print("Total Distance::",totalDistance(P,s))
 
+def Plot(seq,P,dist,Pnames):
+    Pt = [P[seq[i]] for i in range(len(seq))]
+    Pt += [P[seq[0]]]
+    Pt = array(P)
+    title('Total distance='+str(dist))
+    plot(Pt[:,0],Pt[:,1],'-o')
+    for i in range(len(P)):
+    	annotate(Pnames[i],(P[i][0],P[i][1]))
+    show()
+
+Plot(s,P,506.75,Pnames)
