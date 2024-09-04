9f62f4be6326053388aa5ce8461f6ec8564430ac
Stephen George <stephengeorgepune@gmail.com>
2024-09-04 19:14:59 +0530

    Some more updates

--- Stochastic Methods/travellingSalesman(GA).py
+++ Stochastic Methods/travellingSalesman(GA).py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import math,random
 
 def readcities(Pnames):
-    p = [] #co-ordinates of cities
+    citylist = [] #co-ordinates of cities
     j = 0 #counter
     geolocator = Nominatim(user_agent="SG_App")
 
@@ -23,9 +23,9 @@ def readcities(Pnames):
                 #  out.write("City = ",city,pt.latitude,pt.longitude)
                 out.write(f"City = {city} {pt.latitude},{pt.longitude} \n")
                 # out.write('/n')
-                p.insert(j,[pt.latitude,pt.longitude])
+                citylist.insert(j,[pt.latitude,pt.longitude])
                 j += 1
-    return p
+    return citylist
 
 class City:
     def __init__(self,x,y,name):
@@ -46,4 +46,26 @@ class Fitness:
         self.distance = getTotalDistance(self.route)
 
     def getFitness(self):
-        pass
+        self.fitness = 1.0/self.distance
+        return self.fitness
+
+
+def distance(P1,P2):
+    d = math.sqrt((P1[0]-P2[0])**2 + (P1[1]-P2[1])**2)
+    return d
+
+def getTotalDistance(P,seq):
+    dist = 0.0
+    N = len(seq)
+    for i in range(0,N-1):
+        dist += distance(P[seq[i]],P[seq[i+1]])
+
+    dist += distance(P[0],P[seq[N-1]])
+    return dist
+
+def createRoute(citylist):
+    route = random.sample(citylist,len(citylist))
+    return route
+
+def initialPopulation(popSize,citylist):
+    pass
